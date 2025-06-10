de4ed9f0b05efe6e563e61e0024ef73bb947d8d2
divyap-devops <dvp.learn@gmail.com>
2025-06-10 21:23:33 +0530

    Create sample.sh

---
+++ sample.sh
@@ -0,0 +1,7 @@
+read -p "enter first number" n1
+read -p "enter second number" n2
+read -p "enter first number" n3
+if [ $n1 -gt $n2] && [ n1 -gt $n3 ]
+then
+  echo "$n1 biggest number in the list"
+  
